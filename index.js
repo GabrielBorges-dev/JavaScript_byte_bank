d12ad169acba07592d1ca95396d3d77692f98e3b
Gabriel Borges <gsb.gabriel5@gmail.com>
2022-02-15 07:52:13 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ import { Diretor } from "./funcionarios/Diretor.js"
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Lucas", 10000, 12345678900);
-diretor.cadastrarSenha("12345")
+diretor.cadastrarSenha("12345");
 const gerente = new Gerente("Vitor", 5000, 112345612300);
+gerente.cadastrarSenha("555666777");
 
 const estaLogado = SistemaAutenticacao.login(diretor, diretor._senha);
+const estaLogadoGerente = SistemaAutenticacao.login(gerente, gerente._senha);
 
 console.log(diretor)
 console.log(estaLogado);
+console.log(gerente)
+console.log(estaLogadoGerente);
